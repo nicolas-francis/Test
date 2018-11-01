699edfb0355b70f28842c4b9c080b658116780b2
ISdataTest <44676501+ISdataTest@users.noreply.github.com>
2018-11-01 11:41:04 -0400

    2nd commit

---
+++ second/src/app/test/test.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+ //// this is a test for GITHUB
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <div *ngFor="let employee of employees">
+      <h2>{{employee.id}} {{employee.name}}  {{employee.age}}</h2>
+    </div>
+
+
+
+   `,
+  styleUrls: ['./test.component.css']
+})
+
+
+export class TestComponent implements OnInit {
+
+  public hasError = true;
+  //public animals = ["cat","dog","scorpion","bat","lion"];
+  public name="gazel";
+  public help = 'Hello';
+  public employees = [
+      {"id": 1,"name":"dieudo", "age":"25"},
+      {"id": 2,"name":"zaki", "age":"35"},
+      {"id": 3,"name":"nowar", "age":"42"},
+      {"id": 4,"name":"nora", "age":"29"}
+  ];
+
+  public date = new Date();
+  
+  public urlSite = window.location.href;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onclick(){
+      window.console.log(name);
+      //this.animals.push(name);
+  }
+
+}
